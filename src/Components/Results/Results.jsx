c51af5af80c20b698d535cd993ae0d874a89734f
MrGuzior <konrad.guzior@gmail.com>
2020-11-17 15:34:03 +0100

    Result routing

--- src/Components/Results/Results.jsx
+++ src/Components/Results/Results.jsx
@@ -15,11 +15,11 @@ const Results = () => {
     return(
         <section className="results">
             <ul>
-                <li onClick={()=>setActiveList('total')}>Total</li>
-                <li onClick={()=>setActiveList('class1')}>Klass 1</li>
-                <li onClick={()=>setActiveList('class2')}>Klass 2</li>
-                <li onClick={()=>setActiveList('twoSeated')}>Tvåsits</li>
-                <li onClick={()=>setActiveList('junior')}>Junior</li>
+                <li onClick={()=>setActiveList('total')}><Link to='/results/total'>Total</Link></li>
+                <li onClick={()=>setActiveList('class1')}><Link to='/results/class1'>Klass 1</Link></li>
+                <li onClick={()=>setActiveList('class2')}><Link to='/results/class2'>Klass 2</Link></li>
+                <li onClick={()=>setActiveList('twoSeated')}><Link to='/results/twoseated'>Tvåsits</Link></li>
+                <li onClick={()=>setActiveList('junior')}><Link to='/results/junior'>Junior</Link></li>
             </ul>
 
             <ResultList list={activeList}/>
