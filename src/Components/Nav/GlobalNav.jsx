d7984f3cababd639636a5909869bd09cea5ec627
MrGuzior <konrad.guzior@gmail.com>
2020-11-18 12:42:38 +0100

    Collapsable nav menu added, materialui

--- src/Components/Nav/GlobalNav.jsx
+++ src/Components/Nav/GlobalNav.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import {NavLink as Link} from 'react-router-dom'
+import {MenuItem} from '@material-ui/core'
 
 const GlobalNav = () => {
     return(
         <nav className='global-nav navigation'>
             <ul>
-                <li><Link to='/results'>Resultat</Link></li>
-                <li><Link to='/competitions'>Tävlingar</Link></li>
-                <li><Link to='/rules'>Regler</Link></li>
-                <li><Link to='/contact'>Kontakt</Link></li>
+                <li><MenuItem><Link to='/results'>Resultat</Link></MenuItem></li>
+                <li><MenuItem><Link to='/competitions'>Tävlingar</Link></MenuItem></li>
+                <li><MenuItem><Link to='/rules'>Regler</Link></MenuItem></li>
+                <li><MenuItem><Link to='/contact'>Kontakt</Link></MenuItem></li>
             </ul>
         </nav>
     )
