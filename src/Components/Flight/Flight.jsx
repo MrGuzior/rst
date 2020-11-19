9a7b7cd42f6cc8ac5a7b9e7974ca3936f3e9b99e
MrGuzior <konrad.guzior@gmail.com>
2020-11-19 14:50:09 +0100

    Primitive flight display added

--- src/Components/Flight/Flight.jsx
+++ src/Components/Flight/Flight.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import {useParams} from 'react-router-dom'
+import {useSelector, useDispatch} from 'react-redux'
+import {selectData} from '../../store/appSlice'
 
 const Flight = () => {
     const {id} = useParams()
-    console.log(id)
+    const {flights} = useSelector(selectData)
+    const flight = flights.filter(flight => flight.id === id)[0]
+    
     return(
-    <p>{id}</p>
+        <section className='flight'>
+            <p>{flight.pilotName}</p>
+            <p>{flight.details.distance}km</p>
+            <p>{flight.details.speed}km/s</p>
+            <p>{flight.details.height}m</p>
+        </section>
     )
 }
 
