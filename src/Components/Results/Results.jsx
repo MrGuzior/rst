8c12de74f2dba4f3df5340ecfb7bf4fd4535df55
MrGuzior <konrad.guzior@gmail.com>
2020-11-20 15:37:26 +0100

    Responsive result navbar

--- src/Components/Results/Results.jsx
+++ src/Components/Results/Results.jsx
@@ -14,27 +14,40 @@ const Results = () => {
     return(
         <section className="results">
             <div className='d-flex flex-wrap class-select'>
-                <Form.Group className='' controlId="exampleForm.ControlSelect1" >
-                    <Form.Control as="select">
-                    <option>Allmän (Totalt)</option>
-                    <option>Klass 1</option>
-                    <option>Klass 2</option>
-                    <option>Tvåsits</option>
-                    <option>Junior</option>
-                    </Form.Control>
-                </Form.Group>
-                <Navbar  className='p-0 '>
-                    <Nav className="mr-auto" fill variant="pills" >
-                        <Nav.Link className='px-2'>Kombination</Nav.Link>
-                        <Nav.Link className='px-2'>Höjd</Nav.Link>
-                        <Nav.Link className='px-2'>Distans</Nav.Link>
-                        <Nav.Link className='px-1'>Ack Distans</Nav.Link>
-                        <Nav.Link className='px-2'>Hastighet</Nav.Link>
+                
+            <Form.Group className='mr-0 show-when-mobile' controlId="exampleForm.ControlSelect1" >
+                                <Form.Control as="select">
+                                <option>Allmän (Totalt)</option>
+                                <option>Klass 1</option>
+                                <option>Klass 2</option>
+                                <option>Tvåsits</option>
+                                <option>Junior</option>
+                                </Form.Control>
+                            </Form.Group>
+
+                <Navbar  className='p-0'>
+                    <Nav className="mr-auto" fill variant="tabs" >
+                        <Nav>
+                            <Form.Group className='mr-0 hide-when-mobile' controlId="exampleForm.ControlSelect1" >
+                                <Form.Control as="select">
+                                <option>Allmän (Totalt)</option>
+                                <option>Klass 1</option>
+                                <option>Klass 2</option>
+                                <option>Tvåsits</option>
+                                <option>Junior</option>
+                                </Form.Control>
+                            </Form.Group>
+                        </Nav>
+                        <Nav.Link className='px-2' eventKey='kombination'>Kombination</Nav.Link>
+                        <Nav.Link className='px-2' eventKey='height'>Höjd</Nav.Link>
+                        <Nav.Link className='px-2' eventKey='distance'>Distans</Nav.Link>
+                        <Nav.Link className='px-1' eventKey='totalDistance'>Ack Distans</Nav.Link>
+                        <Nav.Link className='px-2' eventKey='speed'>Hastighet</Nav.Link>
                     </Nav>
                 </Navbar>
             </div>
 
-            <div className='result'>
+            <div className='result mt-0 pt-0'>
                 <ResultList list={activeList}/>
             </div>
         </section>
