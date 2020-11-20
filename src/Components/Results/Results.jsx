c4b489ddc1e923ce43fc7b1387258ec4e0339c8d
MrGuzior <konrad.guzior@gmail.com>
2020-11-20 14:08:50 +0100

    Result navbar adjusted

--- src/Components/Results/Results.jsx
+++ src/Components/Results/Results.jsx
@@ -13,30 +13,26 @@ const Results = () => {
 
     return(
         <section className="results">
-
-                    <Form.Group controlId="exampleForm.ControlSelect1">
-                        <Form.Control as="select">
-                        <option>Allmän (Totalt)</option>
-                        <option>Klass 1</option>
-                        <option>Klass 2</option>
-                        <option>Tvåsits</option>
-                        <option>Junior</option>
-                        </Form.Control>
-                    </Form.Group>
-
-            <Navbar className='p-0'>
-                <Nav className="mr-auto " fill variant="pills" >
-
-                    <Nav.Link className='px-2'>Kombination</Nav.Link>
-                    <Nav.Link className='px-2'>Höjd</Nav.Link>
-                    <Nav.Link className='px-2'>Distans</Nav.Link>
-                    <Nav.Link className='px-1'>Ack Distans</Nav.Link>
-                    <Nav.Link className='px-2'>Hastighet</Nav.Link>
-                        
-                        
-
-                </Nav>
-            </Navbar>
+            <div className='d-flex flex-wrap class-select'>
+                <Form.Group className='' controlId="exampleForm.ControlSelect1" >
+                    <Form.Control as="select">
+                    <option>Allmän (Totalt)</option>
+                    <option>Klass 1</option>
+                    <option>Klass 2</option>
+                    <option>Tvåsits</option>
+                    <option>Junior</option>
+                    </Form.Control>
+                </Form.Group>
+                <Navbar  className='p-0 '>
+                    <Nav className="mr-auto" fill variant="pills" >
+                        <Nav.Link className='px-2'>Kombination</Nav.Link>
+                        <Nav.Link className='px-2'>Höjd</Nav.Link>
+                        <Nav.Link className='px-2'>Distans</Nav.Link>
+                        <Nav.Link className='px-1'>Ack Distans</Nav.Link>
+                        <Nav.Link className='px-2'>Hastighet</Nav.Link>
+                    </Nav>
+                </Navbar>
+            </div>
 
             <div className='result'>
                 <ResultList list={activeList}/>
