f7f97280e585ddf69f602c7b8ed4308b983ab74d
MrGuzior <konrad.guzior@gmail.com>
2020-12-02 14:52:50 +0100

    Added more list templates

--- src/db/lists.js
+++ src/db/lists.js
@@ -54,6 +54,231 @@ const resultLists = [
             }
             
         ]
+    },{
+        name: 'Höjdmomentet',
+        path: 'height',
+        tableHeads: [
+            {
+                name: '#',
+                collapsable: true,
+                icon: '',
+                onHover: 'Position',
+                inputType: 'number'
+            },{
+                name: 'Pilot',
+                collapsable: false,
+                icon: '',
+                onHover: 'Pilot namn',
+                inputType: 'string'
+            },{
+                name: 'Klubb',
+                collapsable: true,
+                icon: '',
+                onHover: 'Flygklubb',
+                inputType: 'string'
+            },{
+                name: 'Klass',
+                collapsable: true,
+                icon: '',
+                onHover: 'Tävlingsklass',
+                inputType: 'string'
+            },{
+                name: 'Res 1',
+                collapsable: false,
+                icon: '',
+                onHover: 'Resultat 1',
+                inputType: 'number'
+            },{
+                name: 'Res 2',
+                collapsable: false,
+                icon: '',
+                onHover: 'Resultat 2',
+                inputType: 'number'
+            },{
+                name: 'Res 3',
+                collapsable: false,
+                icon: '',
+                onHover: 'Resultat 3',
+                inputType: 'number'
+            },{
+                name: 'Res 4',
+                collapsable: false,
+                icon: '',
+                onHover: 'Resultat 4',
+                inputType: 'number'
+            },{
+                name: 'Summa',
+                collapsable: false,
+                icon: '',
+                onHover: 'Summa',
+                inputType: 'number'
+            }
+            
+        ]
+    },{
+        name: 'Distansmomentet',
+        path: 'distance',
+        tableHeads: [
+            {
+                name: '#',
+                collapsable: true,
+                icon: '',
+                onHover: 'Position',
+                inputType: 'number'
+            },{
+                name: 'Pilot',
+                collapsable: false,
+                icon: '',
+                onHover: 'Pilot namn',
+                inputType: 'string'
+            },{
+                name: 'Klubb',
+                collapsable: true,
+                icon: '',
+                onHover: 'Flygklubb',
+                inputType: 'string'
+            },{
+                name: 'Klass',
+                collapsable: true,
+                icon: '',
+                onHover: 'Tävlingsklass',
+                inputType: 'string'
+            },{
+                name: 'Dist 1',
+                collapsable: false,
+                icon: '',
+                onHover: 'Distans 1',
+                inputType: 'number'
+            },{
+                name: 'Poäng 1',
+                collapsable: false,
+                icon: '',
+                onHover: 'Poäng 1',
+                inputType: 'number'
+            },
+            {
+                name: 'Dist 2',
+                collapsable: false,
+                icon: '',
+                onHover: 'Distans 2',
+                inputType: 'number'
+            },{
+                name: 'Poäng 2',
+                collapsable: false,
+                icon: '',
+                onHover: 'Poäng 2',
+                inputType: 'number'
+            },{
+                name: 'Summa',
+                collapsable: false,
+                icon: '',
+                onHover: 'Summa',
+                inputType: 'number'
+            }
+        ]
+    },{
+        name: 'Ackumulerad distans',
+        path: 'total-distance',
+        tableHeads: [
+            {
+                name: '#',
+                collapsable: true,
+                icon: '',
+                onHover: 'Position',
+                inputType: 'number'
+            },{
+                name: 'Pilot',
+                collapsable: false,
+                icon: '',
+                onHover: 'Pilot namn',
+                inputType: 'string'
+            },{
+                name: 'Klubb',
+                collapsable: true,
+                icon: '',
+                onHover: 'Flygklubb',
+                inputType: 'string'
+            },{
+                name: 'Klass',
+                collapsable: true,
+                icon: '',
+                onHover: 'Tävlingsklass',
+                inputType: 'string'
+            },{
+                name: 'Dist',
+                collapsable: false,
+                icon: '',
+                onHover: 'Distans',
+                inputType: 'number'
+            },{
+                name: 'Ack. Distans',
+                collapsable: false,
+                icon: '',
+                onHover: 'Ackumulerad distans',
+                inputType: 'number'
+            }
+        ]
+    },{
+        name: 'Hastighetsmomentet',
+        path: 'speed',
+        tableHeads: [
+            {
+                name: '#',
+                collapsable: true,
+                icon: '',
+                onHover: 'Position',
+                inputType: 'number'
+            },{
+                name: 'Pilot',
+                collapsable: false,
+                icon: '',
+                onHover: 'Pilot namn',
+                inputType: 'string'
+            },{
+                name: 'Klubb',
+                collapsable: true,
+                icon: '',
+                onHover: 'Flygklubb',
+                inputType: 'string'
+            },{
+                name: 'Klass',
+                collapsable: true,
+                icon: '',
+                onHover: 'Tävlingsklass',
+                inputType: 'string'
+            },{
+                name: 'Hast 1',
+                collapsable: false,
+                icon: '',
+                onHover: 'Hastighet 1',
+                inputType: 'number'
+            },{
+                name: 'Poäng 1',
+                collapsable: false,
+                icon: '',
+                onHover: 'Poäng 1',
+                inputType: 'number'
+            },
+            {
+                name: 'Hast 2',
+                collapsable: false,
+                icon: '',
+                onHover: 'Hastighet 2',
+                inputType: 'number'
+            },{
+                name: 'Poäng 2',
+                collapsable: false,
+                icon: '',
+                onHover: 'Poäng 2',
+                inputType: 'number'
+            },{
+                name: 'Summa',
+                collapsable: false,
+                icon: '',
+                onHover: 'Summa',
+                inputType: 'number'
+            }
+        ]
     }
 ]
 
